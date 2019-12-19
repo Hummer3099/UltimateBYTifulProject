7d50df4394a50efce7e4c3110e94630995a486fd
Hummer3099 <abeedo_nedal@live.com>
2019-12-19 14:36:45 +0100

    Just initiated

---
+++ BYT11_12/BYT11_12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BYT11_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
