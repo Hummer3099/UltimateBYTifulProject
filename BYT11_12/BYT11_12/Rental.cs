78d19f37f1691f2a179a2dd3fd58ae8aba224b5b
kalinin911 <ikalinin911@gmail.com>
2020-01-15 20:55:17 +0100

    Add files via upload

---
+++ BYT11_12/BYT11_12/Rental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Rental
+{
+    public DateTime RentDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime ReturnDate { get; set; }
+
+	public Rental(DateTime dueDate, DateTime returnDate)
+	{
+        this.RentDate = DateTime.Now;
+        this.DueDate = DateTime.ParseExact(dueDate, "dd/MM/yyyy", null);
+        this.ReturnDate = DateTime.ParseExact(returnDate, "dd/MM/yyyy", null);
+	}
+
+    public void ChangeReturnDate(DateTime newReturnDate)
+    {
+        this.ReturnDate = DateTime.ParseExact(newReturnDate, "dd/MM/yyyy", null);
+    }
+}
