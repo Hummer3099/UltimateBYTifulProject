1f6deebb4d6c5b8c3e539c04fb3ac900eaedfea8
s16717 <56193797+s16717@users.noreply.github.com>
2020-01-15 20:57:49 +0100

    Book and Loss Ticket classes
    hello there

---
+++ BYT11_12/BYT11_12/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BYT11_12
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Descritpion { get; set; }
+        public string Location { get; set; }
+        public string Genre { get; set; }
+        public string BookStatus { get; set; }
+        public Book(string title, string author, string publicationDate, string description, string location, string genre, string bookStatus)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.PublicationDate= DateTime.ParseExact(publicationDate, "dd/MM/yyyy", null); ;
+            this.Descritpion = description;
+            this.Location = location;
+            this.Genre = genre;
+            this.BookStatus = bookStatus;
+        }
+        
+    }
+}
---
+++ BYT11_12/BYT11_12/LossTicket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BYT11_12
+{
+    class LossTicket
+    {
+        public DateTime SubmissionDate { get; set; }
+        public double Fine { get; set; }
+        public LossTicket(string submissionDate, double fine)
+        {
+            this.SubmissionDate = DateTime.ParseExact(submissionDate, "dd/MM/yyyy", null);
+            this.Fine = fine;
+        }
+    }
+}
